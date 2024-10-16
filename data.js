fa08c4111f40015a06f9f41d475a52da3c0970b5
Remax Cony <kevin.espino@remax.es>
2024-10-16 13:51:16 +0100

    Commit

---
+++ data.js
@@ -0,0 +1,469 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-entrada",
+      "name": "ENTRADA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.7639697437555402,
+        "pitch": -0.08626209532644857,
+        "fov": 1.292173873685646
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6143074918136531,
+          "pitch": 0.05196719677843831,
+          "rotation": 0,
+          "target": "1-distribuidor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-distribuidor",
+      "name": "DISTRIBUIDOR",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.8406059402656485,
+        "pitch": 0.03699905799497927,
+        "fov": 1.292173873685646
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.64956495981803,
+          "pitch": 0.054481063733271995,
+          "rotation": 0,
+          "target": "3-cocina"
+        },
+        {
+          "yaw": 2.0196641679841756,
+          "pitch": 0.03363197982090682,
+          "rotation": 0,
+          "target": "4-bao-1"
+        },
+        {
+          "yaw": -2.7518460847586397,
+          "pitch": -0.022088828842331765,
+          "rotation": 0,
+          "target": "5-2a-planta"
+        },
+        {
+          "yaw": -1.3425322543089706,
+          "pitch": 0.033433679591194476,
+          "rotation": 0,
+          "target": "2-salon"
+        },
+        {
+          "yaw": 0.0854779137578241,
+          "pitch": 0.031035869347043388,
+          "rotation": 0,
+          "target": "0-entrada"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-salon",
+      "name": "SALON",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.746167718241571,
+        "pitch": -0.0002501928577416379,
+        "fov": 1.292173873685646
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.459772460581771,
+          "pitch": 0.0808389490530459,
+          "rotation": 0,
+          "target": "1-distribuidor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-cocina",
+      "name": "COCINA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -1.1268308846852388,
+        "pitch": 0.06865773988246104,
+        "fov": 1.292173873685646
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.008920399553295155,
+          "pitch": 0.07128710610005307,
+          "rotation": 0,
+          "target": "1-distribuidor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-bao-1",
+      "name": "BAÑO 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 3.0693974319393096,
+        "pitch": -0.042123496958044626,
+        "fov": 1.292173873685646
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.773853704784857,
+          "pitch": 0.08445038282775741,
+          "rotation": 0,
+          "target": "3-cocina"
+        },
+        {
+          "yaw": -0.15194934829939655,
+          "pitch": 0.11199632473618948,
+          "rotation": 0,
+          "target": "5-2a-planta"
+        },
+        {
+          "yaw": 0.8264802733805858,
+          "pitch": 0.053465885577729466,
+          "rotation": 0,
+          "target": "2-salon"
+        },
+        {
+          "yaw": 0.41262796359523257,
+          "pitch": 0.09705866685581377,
+          "rotation": 3.141592653589793,
+          "target": "1-distribuidor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-2a-planta",
+      "name": "2a PLANTA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.836908762928916,
+        "pitch": 0.07858110986186517,
+        "fov": 1.292173873685646
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.7919768256090567,
+          "pitch": 0.03954639080508926,
+          "rotation": 0,
+          "target": "9-bao-2"
+        },
+        {
+          "yaw": -2.6639785948367436,
+          "pitch": 0.0409946616459127,
+          "rotation": 0,
+          "target": "7-hab-2"
+        },
+        {
+          "yaw": 0.9626751647387817,
+          "pitch": 0.09952747302479992,
+          "rotation": 0,
+          "target": "6-hab-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-hab-1",
+      "name": "HAB 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.596209081140934,
+        "pitch": 0.051866950057107175,
+        "fov": 1.292173873685646
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.869764373589577,
+          "pitch": 0.052334666120763984,
+          "rotation": 0,
+          "target": "5-2a-planta"
+        },
+        {
+          "yaw": -3.1067893195277048,
+          "pitch": 0.016848667289268704,
+          "rotation": 0,
+          "target": "7-hab-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-hab-2",
+      "name": "HAB 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.7719801459998177,
+        "pitch": 0.038475211914326124,
+        "fov": 1.292173873685646
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5397350618885337,
+          "pitch": 0.00699217961360965,
+          "rotation": 0,
+          "target": "5-2a-planta"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-hab-3",
+      "name": "HAB 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -3.0175957905234867,
+        "pitch": 0.04575617383635944,
+        "fov": 1.292173873685646
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.803248698663383,
+          "pitch": 0.012599015281695713,
+          "rotation": 0,
+          "target": "5-2a-planta"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-bao-2",
+      "name": "BAÑO 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -3.035179700116048,
+        "pitch": 0.027530724787384386,
+        "fov": 1.292173873685646
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.41563588887174774,
+          "pitch": 0.0947085106193466,
+          "rotation": 3.141592653589793,
+          "target": "5-2a-planta"
+        },
+        {
+          "yaw": -0.0527050827055664,
+          "pitch": 0.08632973933145749,
+          "rotation": 0,
+          "target": "6-hab-1"
+        },
+        {
+          "yaw": -1.4645975240124756,
+          "pitch": 0.10150821237453655,
+          "rotation": 0,
+          "target": "7-hab-2"
+        },
+        {
+          "yaw": 0.4138883805227973,
+          "pitch": 0.06010094960277357,
+          "rotation": 0,
+          "target": "8-hab-3"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
